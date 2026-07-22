34f1875949295e277c02841887876454bd5d2441
Shagufta Ahmed <shaguftaahmed260@gmail.com>
2026-07-22 17:52:38 +0530

    Add inverted pyramid and diamond patterns

---
+++ Patterns/pattern_8.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+void pattern8(int n){
+    for (int i=1; i<=n; i++){
+        for (int j=1;j<= i - 1; j++){
+            cout << " ";
+        }
+        for (int j=1; j< 2 * n - 2 * i + 1; j++){
+            cout << "*";
+        }
+        cout << endl;
+    }
+}
+int main(){
+    int n;
+    cin >> n;
+    pattern8(n);
+    return 0;
+}
