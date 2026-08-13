c1b5c0303b21a225e63ed7f0488feb51e461b92d
Shagufta Ahmed <shaguftaahmed260@gmail.com>
2026-08-14 00:09:04 +0530

    move zeros to end

---
+++ Revisions/Arrays/move_zero_to_End.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+void movetoEnd(vector <int> &arr){
+    int j = -1;
+    for (int i = 0; i < arr.size(); i ++){
+        if ( arr[i] == 0){
+            j = i;
+            break;
+        }
+    }
+    for (int i = j + 1; i < arr.size(); i++){
+        if ( arr[i] != 0){
+            swap (arr[i], arr[j]);
+            j ++;
+        }
+    }
+}
+int main(){
+    vector <int> arr = { 1,0,0,2,0,3,0,5,6,0,7,0,3};
+    movetoEnd(arr);
+    for (int i = 0; i < arr.size(); i++){
+        cout << arr[i] << " ";
+    }
+    return 0;
+}
